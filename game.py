04e745ef782fb3be0a159af815bf7ae2f4be6a18
Jacqueline Espinosa <jacquelineespinosa@imac.myfiosgateway.com>
2022-11-18 09:11:20 -0500

    added method for number of players

--- game.py
+++ game.py
@@ -12,4 +12,17 @@ class Game:
         self.rules = 'Here are the rules of the game: In a round of three games you would select a number according to which item you would like to be. Out of those three games you would be the winner according to the number of rounds you won. Choose wisely, the game can be quite vicious, particularly with the opponent chosen! Good luck!!'
         pass
 
-    
+    def num_of_players(self):
+        while True:
+            try:
+                num = int(input("How many will be playing? Enter 1, 2, or 3. "))
+            except ValueError:
+                print("There was an error, please enter a single digit number. ")
+                continue
+            if num <4 and num > 0:
+                print(num)
+                break
+            else:
+                print("There was an error, please enter a single digit number. ")
+
+                
