a7c76cdd7972e25e9451d514bc0465cfc54130f8
Jacqueline Espinosa <jacquelineespinosa@imac.myfiosgateway.com>
2022-11-18 09:06:38 -0500

    created method for welcome and the rules

--- game.py
+++ game.py
@@ -7,4 +7,9 @@ class Game:
         self.ai = Ai('Computer_one')
         pass
 
+    def intro_message(self):
+        self.welcome = 'Welcome to Rock, Scissor, Spock, and Lizard!'
+        self.rules = 'Here are the rules of the game: In a round of three games you would select a number according to which item you would like to be. Out of those three games you would be the winner according to the number of rounds you won. Choose wisely, the game can be quite vicious, particularly with the opponent chosen! Good luck!!'
+        pass
+
     
